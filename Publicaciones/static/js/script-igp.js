4229138314c2b8ddcfce93c7d8f1d62223c5a94a
RonaldNestor <ronald4261@hotmail.com>
2014-03-05 17:19:36 -0500

    perfil investigadores

---
+++ Publicaciones/static/js/script-igp.js
@@ -0,0 +1,40 @@
+$(function(){
+  $(".nav-sidebar a").click(function(e){
+    $(".nav-sidebar li").removeClass("active");
+    $(this).parent("li").addClass("active");
+    conte = $(this).attr("href")
+    $(".reser").hide();
+    $(conte).show();
+  })
+
+  // if (window.location.hash) {
+  //     idconte = $('div '+window.location.hash);
+  //     // href = $("a[href="+window.location.hash+"]")
+  //     $("a[href="+window.location.hash+"]").click();
+  //     idconte.show();
+  // }
+  // else{
+  //     $(".reser a:first").click();
+  // }
+
+  // $( '.calendar' ).datepicker({
+  //     showOn: 'button',
+  //     buttonImage: './img/calendar.gif',
+  //     buttonImageOnly: true,
+  //     changeMonth: true,
+  //     changeYear: true,
+  //     dateFormat:'yy-mm-dd',
+  //     //minDate: '-15Y', maxDate: '+1M +10D',
+  //     yearRange: '-70:+0'
+  // }).mask('9999-99-99');
+
+  // var f = new Date();
+  // dia =f.getDate();
+  // mes =f.getMonth() +1;
+  // if (dia <=9) {dia="0"+dia};
+  // if (mes <=9) {mes="0"+mes};
+  // console.log(f.getDate());
+  // fx_today=f.getFullYear() + "-" + mes + "-" + dia;
+  // $(".fx_today").val(fx_today);
+  $(".login-home input").addClass("span12");
+});
--- Publicaciones/views.py
+++ Publicaciones/views.py
@@ -12,7 +12,27 @@ from django.contrib.auth import login, logout, authenticate
 
 def home(request):
     # Recuperar 10 investigadores como minimo
-    return render_to_response('home.html',context_instance=RequestContext(request))
+    mensaje = ""
+    if request.user.is_authenticated():
+        # return HttpResponseRedirect('/')
+        return render_to_response('home.html',context_instance=RequestContext(request))
+    else:
+        if request.method == "POST":
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                next = request.POST['next']
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                usuario = authenticate(username=username,password=password)
+                if usuario is not None and usuario.is_active:
+                    login(request,usuario)
+                    return HttpResponseRedirect(next)
+                else:
+                    mensaje = "usuario y/o password incorrecto"
+        next = request.REQUEST.get('next')
+        form = LoginForm()
+        ctx = {'form':form,'mensaje':mensaje,'next':next}
+        return render_to_response('home.html',ctx,context_instance=RequestContext(request))
 
 def investigadores(request):
     inv = Investigador.objects.all()[:10]
